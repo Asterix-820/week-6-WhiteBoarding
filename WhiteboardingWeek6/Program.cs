dff857bab26b8a808cb6884f35909d8d0d81e81d
John Ndukwe <johnndukwe820@gmail.com>
2023-02-19 23:52:58 +0100

    week 6 task done

---
+++ WhiteboardingWeek6/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Mysolution
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Stack<int> N = new Stack<int>();
+            N.Push(1);
+            N.Push(2);
+            N.Push(4);
+            N.Push(3);
+            N.Push(3);
+            N.Push(5);  
+
+            Console.WriteLine(StackOp(N,7));
+        }
+
+        public static int StackOp(Stack<int> stack, int k)
+        {
+            int count = 0;
+
+            while (count < k - 1 && stack.Count> 0) 
+            {
+                stack.Pop();
+                count++;
+            }
+
+            if (stack.Count == 0) 
+            {
+                return -1;
+            }
+            return stack.Peek();
+        }
+    }
+}
